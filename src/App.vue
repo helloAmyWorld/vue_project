84d6c712a3261bf8d2793b025a70f14f348970e1
hellAmyWorld <278108044@qq.com>
2018-07-15 17:54:53 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-pagination :total="total" :current-page='current' @pagechange="pagechange"></v-pagination>
+</template>
+
+<script type="es6">
+  import pagination from './components/pagination'
+  export default{
+    data() {
+      return {
+        total: 80,     // 记录总条数
+        display: 10,   // 每页显示条数
+        current: 1,   // 当前的页数
+      }
+    },
+        methods: {
+        pagechange:function(currentPage){
+          console.log(currentPage);
+          // ajax请求, 向后台发送 currentPage, 来获取对应的数据
+        }
+      },
+      components: {
+        'v-pagination': pagination
+      }
+    }
+</script>
